02c3663fca73272754517f0892280a6fa6ffcc89
maahzaade <mahdiye.nikraftar@gmail.com>
2018-11-08 09:26:40 -0500

    injecting array of String in HappyFortuneService

--- src/com/luv2code/springdemo/HappyFortuneService.java
+++ src/com/luv2code/springdemo/HappyFortuneService.java
@@ -1,7 +1,9 @@
 package com.luv2code.springdemo;
 
 public class HappyFortuneService implements FortuneService {
-     private String extraVar;
+    public final int MAX_ELEMENT = 2;
+    private String extraVar;
+    private String[] dayFortunes;
 
     public HappyFortuneService(String extraVar) {
         this.extraVar = extraVar;
@@ -9,6 +11,14 @@ public class HappyFortuneService implements FortuneService {
 
     @Override
     public String getFortune() {
-        return "Today is your lucky day! " + extraVar;
+        int element = (int) ((Math.random()) * (MAX_ELEMENT + 1));
+
+        return dayFortunes[element] + " " + extraVar;
+
+    }
+
+    public void setDayFortunes(String[] inputDayFortunes) {
+
+        this.dayFortunes = inputDayFortunes;
     }
 }
